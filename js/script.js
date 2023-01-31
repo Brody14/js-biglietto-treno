e7102f9e4967e30ef01c67920e69e91bf96a53ee
Brody14 <stefania.spinas@gmail.com>
2023-01-31 14:40:41 +0100

    FEAT 2 prompt

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ let km = prompt('Quanti km devi percorrere?');
 console.log(km);
 
 // 2. chiedere all'utente la sua età
+
+let age = prompt('Quanti anni hai?');
+console.log(age);
 // 3. calcolare il prezzo del biglietto in base ai km da percorrere
 // 4. calcolare lo sconto, se previsto
 // 5. dare un forma umana
