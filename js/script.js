adb3f6126c4e1800d0995449f7e28692afc4eb4b
Brody14 <stefania.spinas@gmail.com>
2023-01-31 14:39:19 +0100

    FEAT 1 prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Campazzo')
+console.log('Campazzo');
+
+// 1. chiedere all'utente i km da percorre
+
+let km = prompt('Quanti km devi percorrere?');
+console.log(km);
+
+// 2. chiedere all'utente la sua età
+// 3. calcolare il prezzo del biglietto in base ai km da percorrere
+// 4. calcolare lo sconto, se previsto
+// 5. dare un forma umana
+// 6. stampare il risultato
